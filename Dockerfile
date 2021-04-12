3e0554950b036b43fb03ebda5fbaa1d7e480e1f3
Андрей Саядов <andrejxz3@gmail.com>
2021-04-12 15:59:33 +0700

    Обновлена сборка

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
+FROM node:13.12.0-alpine as build
+WORKDIR /app
+ARG REACT_APP_CATS_API_URL
+ARG REACT_APP_REACTION_URL
+ARG REACT_APP_PHOTOS_API_URL
+ENV PATH /app/node_modules/.bin:$PATH
+COPY package.json ./
+COPY package-lock.json ./
+RUN npm ci --silent
+RUN npm install react-scripts@3.4.1 -g --silent
+COPY . ./
+RUN npm run build
+
 FROM nginx:stable
-
 COPY ./nginx.conf /etc/nginx/conf.d/nginx.conf
-
-COPY build/ /var/www/
+COPY --from=build /app/build/ /var/www/
--- src/config.js
+++ src/config.js
@@ -1,5 +1,15 @@
-export const urls = {
-  catsApi: process.env.CATS_API_URL,
-  reactionApi: process.env.REACTION_API_URL,
-  photosApi: process.env.PHOTOS_API_URL,
+const local = {
+  catsApi: 'http://localhost:3003',
+  reactionApi: 'http://localhost:3002',
+  photosApi: 'http://localhost:3001',
 };
+const production = {
+  catsApi: process.env.REACT_APP_CATS_API_URL,
+  reactionApi: process.env.REACT_APP_REACTION_URL,
+  photosApi: process.env.REACT_APP_PHOTOS_API_URL,
+};
+
+export const urls =
+  process.env.REACT_APP_API_PROD || process.env.NODE_ENV === 'production'
+    ? production
+    : local;

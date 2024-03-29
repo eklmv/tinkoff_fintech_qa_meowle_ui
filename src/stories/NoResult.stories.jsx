443662b4dc2cc5e39d7581c4251f6ebf8a2f1a75
Evgenii Klimov <eklmv.mail@gmail.com>
2024-03-29 22:03:00 +0300

    test: add stories for NoResult component

---
+++ src/stories/NoResult.stories.jsx
@@ -0,0 +1,19 @@
+import { NoResults } from '../common/components/no-results/no-result';
+
+export default {
+    title: 'Components/NoResults',
+    component: NoResults,
+};
+
+export const ShortText = {
+    args: {
+        text: 'Short Text',
+    },
+};
+
+export const LongText = {
+    args: {
+        text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla imperdiet, enim in rutrum suscipit, quam tortor vehicula libero, eget porta tortor enim vel mi.Curabitur in ipsum laoreet, tincidunt sapien ut, imperdiet lectus.Fusce a purus eros. Quisque vitae fermentum nulla, id ultricies nibh. Phasellus gravida ipsum non lectus finibus, et laoreet turpis egestas.Suspendisse a erat magna.Morbi nec quam arcu.Phasellus feugiat lacus id malesuada lobortis.Duis luctus justo vel lacus lobortis suscipit.Integer consectetur vestibulum nibh, ac vulputate orci viverra sit amet.Quisque faucibus nibh facilisis nisi mollis condimentum.Cras quis vehicula odio, eu sagittis felis.Quisque blandit varius dapibus. ',
+    },
+};
